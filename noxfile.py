7614374527f8d4e2e917fb1c371c280e12e91ff9
Stuart Axon <stuaxo2@yahoo.com>
2021-10-05 17:22:13 +0100

    Initial addition of Nox.

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+# noxfile.py
+import nox
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)
+
